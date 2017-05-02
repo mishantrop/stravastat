0e00781ab39cf74eddb77ab99c365d07bf9ece23
misha8000 <quasi-art@ya.ru>
2017-05-03 00:46:58 +0300

    Вывод таблицы тренировок.

--- main.php
+++ main.php
@@ -21,11 +21,10 @@ try {
 	
     $club = $client->getClub($preset['CLUB_ID']); // Velo-Sokol
 	echo '<h2>Клуб '.$club['name'].'</h2>';
-    //echo '<pre>'.print_r($club, true).'</pre>';
+    //
     
-	$clubMembers = $client->getClubMembers($preset['CLUB_ID']);
-	echo '<h2>Участники</h2>';
-    //echo '<pre>'.print_r($clubMembers, true).'</pre>';
+	$clubMembers = $client->getClubMembers($preset['CLUB_ID'], 1, 200);
+	echo '<h2>Участники ('.count($clubMembers).')</h2>';
 	
 	echo '<ul>';
 	foreach ($clubMembers as $clubMember) {
@@ -42,7 +41,25 @@ try {
     
 	$clubActivities = $client->getClubActivities($preset['CLUB_ID'], NULL, 200);
 	echo '<h2>Последние тренировки клуба ('.count($clubActivities).')</h2>';
-    echo '<pre>'.print_r($clubActivities, true).'</pre>';
+	echo '<table>';
+	echo '<tr>
+		<td>Название</td>
+		<td>Дистанция, м</td>
+		<td>Максимальная скорость, м/с</td>
+	</tr>';
+	foreach ($clubActivities as $clubActivity) {
+		echo '<tr>
+			<td>'.$clubActivity['name'].'</td>
+			<td>'.$clubActivity['distance'].'</td>
+			<td>'.$clubActivity['max_speed'].' м/с</td>
+		</tr>';
+	}
+	echo '</table>';
+    
+	echo '<h2>Исходные данные</h2>';
+	echo '<pre>'.print_r($club, true).'</pre>';
+	echo '<pre>'.print_r($clubMembers, true).'</pre>';
+	echo '<pre>'.print_r($clubActivities, true).'</pre>';
 	
 	
 } catch(Exception $e) {
